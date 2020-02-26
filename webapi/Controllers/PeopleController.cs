f682eaee11d5f8b55c1093de6856a41d7b1d4093
Matthew Smalley <matthew.smalley@icgam.com>
2020-02-26 15:08:28 +0000

    Add project files.

---
+++ webapi/Controllers/PeopleController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webapi
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PeopleController
+    {
+        private List<Person> _people = new List<Person>();
+
+        public PeopleController()
+        {
+            _people.Add(new Person{Name = "matthew", Role = "PM"});
+            _people.Add(new Person { Name = "mo", Role = "Dev" });
+        }
+
+        [HttpGet]
+        public IEnumerable<Person> Get()
+        {
+            return _people;
+        }
+    }
+}
